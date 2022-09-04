1fa02c5c78306ede5206969ac92aa1777ce14fc5
KATAROTA <112807542+KATAROTA@users.noreply.github.com>
2022-09-04 07:12:43 -0600

    Add files via upload

---
+++ first.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+//оно работает, не знаю как, но работает
+int FuncMth (int a, int b)	{
+	cout<<"a-b = "<<a-b <<endl;
+	cout<< "a+b = "<<a+b <<endl;
+	cout<< "a*b = "<<a*b <<endl;
+	cout<< "a/b = "<<a/b <<endl;
+	cout<< " " <<endl;
+	return 0;
+ }
+
+int main() {
+	FuncMth (10, 2);
+	FuncMth (15, 1);
+	return 0;
+}
+
+
+
+
+
